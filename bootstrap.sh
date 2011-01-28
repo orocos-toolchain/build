4f91b390f84f3efc7e0cb5c0b82e272d37edab7e
Peter Soetens <peter@thesourceworks.com>
2011-01-28 14:47:10 +0100

    bootstrap: detect bootstrapping in ROS_ROOT environments
    Abort if not in ROS_PACKAGE_PATH
    
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+if [ -n "$ROS_ROOT" ]; then
+   for i in $(echo $ROS_PACKAGE_PATH | sed -e's/:/ /g'); do if expr match "`pwd`" "$i"; then path_ok=1; fi; done > /dev/null
+   if [ -n $path_ok  ]; then
+       echo "Error: ROS_ROOT detected, but '$(pwd)' is not in your ROS_PACKAGE_PATH"
+       echo " Please run this script in a sub-directory of ROS_PACKAGE_PATH (='$ROS_PACKAGE_PATH')"
+       echo " or unset ROS_ROOT in order not to bootstrap for ROS."
+       exit 1
+   fi
+fi
+
 if ! test -f $PWD/autoproj_bootstrap; then
     if which wget > /dev/null; then
         DOWNLOADER=wget
