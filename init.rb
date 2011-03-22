6f021c48b5462e07f6c7ad7366381ab032beee4b
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-03-22 16:27:54 +0100

    use the gitorious configuration handling shipped with autoproj itself

--- init.rb
+++ init.rb
@@ -38,30 +38,6 @@ ignore(/\.sw?$/)
 # Ignore the numerous backup files
 ignore(/~$/)
 
-#
-# Adds the GITORIOUS option to autoproj.
-# Set using the config.yml file.
-#
-gitorious_long_doc = [
-    "Access method to gitorious (git, http or ssh)",
-    "Use 'ssh' only if you have a gitorious account and have commit rights",
-    "on the Orocos projects. Otherwise, we advise you to use 'git'"]
-
-configuration_option 'GITORIOUS', 'string',
-    :default => "git",
-    :values => ["http", "ssh"],
-    :doc => gitorious_long_doc do |value|
-
-    if value == "git"
-        Autoproj.change_option("GITORIOUS_ROOT", "git://gitorious.org/")
-    elsif value == "http"
-        Autoproj.change_option("GITORIOUS_ROOT", "http://git.gitorious.org/")
-    elsif value == "ssh"
-        Autoproj.change_option("GITORIOUS_ROOT", "git@gitorious.com:")
-    end
-
-    value
-end
-
-Autoproj.user_config('GITORIOUS')
+require 'autoproj/gitorious'
+Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 
