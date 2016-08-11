a1f3234b90092321ac23f911e77fee15695116da
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-08-10 21:22:49 -0300

    use orogen and typelib master to get castxml support

--- overrides.rb
+++ overrides.rb
@@ -15,3 +15,12 @@ env_set 'TYPELIB_USE_GCCXML', '1'
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 Autoproj.env_add_path 'CMAKE_PREFIX_PATH', Autobuild::Package['rtt'].prefix
 
+Autoproj.manifest.each_autobuild_package do |pkg|
+    if pkg.kind_of?(Autobuild::Orogen)
+        if pkg.name != 'std'
+            pkg.depends_on 'std'
+            pkg.orogen_options << '--import=std'
+        end
+    end
+end
+
